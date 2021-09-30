b06f6a6429974220adb8abf5dc5c52262e48c38f
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-09-30 22:59:08 +0700

    rest-api

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,24 +3,28 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
-
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+	export class UsersService {
+		create(createUserDto: CreateUserDto) {
+		return 'User account successfuly created';
 }
+
+	//Get data all users
+	findAll() {
+		return `Successfuly get all user`;
+	}
+
+	//Find selected user with id  ||  find user by id
+	findOne(id: number) {
+		return `Data User #${ id } successfuly founded`;
+	}
+
+	//Update data selected user by user id
+	update(id: number, updateUserDto: UpdateUserDto) {
+		return `Data User #${id} successfuly updated`;
+	}
+
+	//Delete data user by id
+	remove(id: number) {
+			return `Data User #${id} successfuly deleted`;
+		}
+	}
