830356d1b8360ccab1c191f70c6728c421c4592d
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-09-30 20:55:43 +0700

    main setup and config

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+// import dotenv
+import "dotenv/config"
+
+// import PORT
+const PORT = process.env.PORT
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT);
+
+  // Write in log if server runnning
+  Logger.log(`Server running on port ${ PORT } `, "Server Running")
+}
+
+bootstrap();
