d0e892c8c122393a27cf1f6ccbd7d32071420ecf
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-10-05 23:17:12 +0700

    delete

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Req, Res, Param } from '@nestjs/common';
+import { Request, Response } from 'express'
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto'
 
@@ -6,11 +7,20 @@ import { CreateProductDto } from './dto/create-product.dto'
 export class ProductController {
        constructor(private readonly productService: ProductService) {}
        @Get()
-		findAll() : string {
-			return `Hello Product`
+		findAll(@Req() req: Request, @Res() res: Response) : Response {
+                     console.log(req.url)
+			return res.send('Hello this is Request')
 		}
+       @Get(":id")
+              findOne(@Param("id") id) : string {
+                     return `Product with id ${id}`
+              }
 	@Post()
 		create(@Body() productDtoParams: CreateProductDto) {
 			return `Data Product : ${productDtoParams.name} and quantity : ${productDtoParams.quantity}`
 		}
+       @Delete(":id")
+       deleteProduct(@Param("id") id) : string {
+              return `Data Product ${id} successfuly deleted`
+       }
 }
