839429aa509aec5611e431b41c982dd935f85e08
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-10-07 22:31:33 +0700

    multipleCapture

--- checkk/multipleCapture.js
+++ checkk/multipleCapture.js
@@ -1,26 +1,33 @@
 const puppeteer = require("puppeteer")
 const fs = require("fs")
-    // const multiple = fs.readFileSync("pages.json", "utf-8")
+const pages = fs.readFileSync('./checkk/pages.json', 'utf-8')
+
+const site = JSON.parse(pages)
 
 async function rapidScreenshot() {
-    if (!fs.existsSync("company")) {
-        fs.mkdirSync("company")
+    try {
+        for (let i = 0; i < site.length; i++) {
+            const browser = await puppeteer.launch({
+                headless: false,
+                defaultViewport: {
+                    height: 800,
+                    width: 1620
+                },
+                slowMo: 8
+            })
+            const page = await browser.newPage()
+            await page.goto(site[i].appLinks, { waitUntil: 'load' })
+
+            await page.screenshot({ path: `./company/${site[i].title}.jpg`, fullPage: false, type: "jpeg" })
+
+            await page.close()
+            await browser.close()
+        }
+    } catch (error) {
+        console.log(`Error :  ${error}`)
+    } finally {
+        console.log(`All page successfully captured`)
     }
-    const browser = await puppeteer.launch({
-        headless: false,
-        defaultViewport: {
-            height: 800,
-            width: 1920
-        },
-        slowMo: 8
-    })
-    const page = await browser.newPage()
-    await page.goto('https://www.google.com', { waitUntil: 'load' })
-
-    await page.screenshot({ path: `./company/google.jpg`, fullPage: false })
-
-    await page.close()
-    await browser.close()
 }
 
 rapidScreenshot()
